f44054b811768a2c577550efa86f1158f19ffb28
TeenKanisorn <kanisornp.dev@gmail.com>
2024-07-06 17:17:27 +0700

    Update Basic Axios

---
+++ axios/src/App.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+function App() {
+  const urlApi = "https://jsonplaceholder.typicode.com/posts"
+  const [data, setData] = useState([])
+
+  const handleGet = ()=> {
+    axios.get(`${urlApi}/1`).then(response => {setData(response.data)})
+  }
+
+  const handlePost = ()=> {
+    axios.post(`${urlApi}`, {
+      title: "AA TT KAI",
+      body: "kakakakakakakakakaa"
+    }).then(response => {setData(response.data)})
+  }
+
+  const handlePut = ()=> {
+    axios.put(`${urlApi}/1`, {
+      title: "AA Mamipoko",
+      body: "AA Paaom deknoi kai kak plakapong"
+    }).then(response => {setData(response.data)})
+  }
+
+  const handleDelete = ()=> {
+    axios.delete(`${urlApi}/1`)
+    .then(() => {
+      setData([])
+      // alert("Delete Complete")
+    })
+  }
+
+  return (
+    <div align="center">
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+      <button onClick={handleGet}>GET</button>
+      <button onClick={handlePost}>Post</button>
+      <button onClick={handlePut}>Put</button>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+}
+
+export default App;
